2f9d266fffe68653639cd480d1f1e999190790d2
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-02 16:38:17 -0300

    feat: adding fastify-type-provider-zod dependency and setting up

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import fastify, { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-    log: ['query']
-})
-
-app.post('/events', async (
-    req: FastifyRequest,
-    reply: FastifyReply
-) => {
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable()
-    })
-    const eventData = createEventSchema.parse(req.body)
-
-    const event = await prisma.event.create({
-        data: {
-            title: eventData.title,
-            details: eventData.details,
-            maximumAttendees: eventData.maximumAttendees,
-            slug: new Date().toISOString(),
-        }
-    })
-
-    return reply.status(201).send({ eventId: event.id })
-})
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.listen({ port: 3333 }, () => {
     console.log("🔥 Server Running 🚀☕")
