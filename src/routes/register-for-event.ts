42645db961b54dbda466e41eff76daac5c90d885
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-02 17:31:05 -0300

    feat: Validating if attendee is already register in event and if event has already reached to maximum attendees

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -27,6 +27,36 @@ export async function registerForEvent(app: FastifyInstance) {
             const { eventId } = req.params
             const attendeeData = req.body
 
+            const attendeeFromEmail = await prisma.attendee.findUnique({
+                where: {
+                    eventId_email: {
+                        email: attendeeData.email,
+                        eventId
+                    }
+                }
+            })
+
+            if (attendeeFromEmail) {
+                throw new Error('This e-mail is already registered for this event.')
+            }
+
+            const [event, amountOfAttendeesForEvent] = await Promise.all([
+                prisma.event.findUnique({
+                    where: {
+                        id: eventId
+                    }
+                }),
+                prisma.attendee.count({
+                    where: {
+                        eventId
+                    }
+                })
+            ])
+
+            if (event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees) {
+                throw new Error("The maximum number of attendees for this event has been reached.")
+            }
+
             const attendee = await prisma.attendee.create({
                 data: {
                     name: attendeeData.name,
