fdf8c863bc2280484c6f4f09925a682e0eba6609
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-02 17:30:44 -0300

    feat: Adding getEvent route

---
+++ src/routes/get-event.ts
@@ -0,0 +1,61 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function getEvent(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>()
+        .get('/events/:eventId', {
+            schema: {
+                params: z.object({
+                    eventId: z.string().uuid()
+                }),
+                response: {
+                    200: {
+                        event: z.object({
+                            id: z.string(),
+                            title: z.string(),
+                            details: z.string().nullable(),
+                            slug: z.string(),
+                            maximumAtttendees: z.number().int().nullable(),
+                            attendeesAmount: z.number().int(),
+                        })
+                    }
+                },
+            }
+        }, async (req, reply) => {
+            const { eventId } = req.params
+
+            const event = await prisma.event.findUnique({
+                select: {
+                    id: true,
+                    title: true,
+                    details: true,
+                    slug: true,
+                    maximumAttendees: true,
+                    _count: {
+                        select: {
+                            attendees: true,
+                        }
+                    }
+                },
+                where: {
+                    id: eventId
+                }
+            })
+            if (!event) {
+                throw new Error('Event not found.')
+            }
+
+            return reply.send({
+                event: {
+                    id: event.id,
+                    title: event.title,
+                    details: event.details,
+                    slug: event.slug,
+                    maximumAtttendees: event.maximumAttendees,
+                    attendeesAmount: event._count.attendees,
+                }
+            })
+        })
+}
