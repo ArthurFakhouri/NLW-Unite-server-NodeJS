ffe60075d12e4f1e7b8a893c3a2c94cb8c71db0b
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-03 13:22:33 -0300

    feat: Adding @fastify/swagger and @fastify/swagger-ui dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import fastify from 'fastify'
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event';
 import { getEvent } from './routes/get-event';
 import { getAttendeeBadge } from './routes/get-attendee-badge';
 
 const app = fastify()
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        info: {
+            title: 'pass.in',
+            description: 'Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.',
+            version: '1.0.0'
+        }
+    },
+    transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+    routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
