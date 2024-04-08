de28cc7a144a2752ac8ef77267c75daba36c314a
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-04-07 22:46:23 -0300

    feat - adding faker dependency and populating our seed with some values

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import { prisma } from '../src/lib/prisma'
+import { faker } from '@faker-js/faker'
 
 async function seed() {
+    await prisma.event.deleteMany()
+    await prisma.attendee.deleteMany()
+
     await prisma.event.create({
         data: {
             id: 'ad34a061-2e35-4b99-a5aa-a8e778f04d73',
@@ -10,8 +14,18 @@ async function seed() {
             maximumAttendees: 120,
         }
     })
+
+    await Promise.all(Array.from({ length: 112 }).map(async (_) => {
+        await prisma.attendee.create({
+            data: {
+                name: faker.person.fullName(),
+                email: faker.internet.email().toLocaleLowerCase(),
+                eventId: "ad34a061-2e35-4b99-a5aa-a8e778f04d73"
+            }
+        })
+    }))
 }
 
 seed().then(() => {
-    console.log('Seeding complete!')
+    console.log('🌱 Seeding complete!')
 })
